9da43acbaca44ecd30c051159f4bd5e688478edd
thomasfouan <thomasfouan@hotmail.fr>
2017-05-18 11:03:14 +0200

    Initial Commit

---
+++ SharpeApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SharpeApp
+//
+//  Created by Thomas Fouan on 18/05/2017.
+//  Copyright © 2017 Thomas Fouan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
