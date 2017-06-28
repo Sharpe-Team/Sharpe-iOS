1859735bf988d75bfaf3d9f1d10672ee8bd527bf
etudiant <etudiant@iMac-de-etudiant-2.local>
2017-06-28 10:31:21 +0200

    Add login page

---
+++ SharpeApp/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.swift
+//  SharpeApp
+//
+//  Created by etudiant on 28/06/2017.
+//  Copyright © 2017 Thomas Fouan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet var loginTextView: UITextField!
+    @IBOutlet var passwordTextView: UITextField!
+    @IBOutlet var connectionBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
